d14fa981db049896869c879a6dc7f9f2b895fb32
Steve weaver Crawford <steve@australiansteve.com>
2016-11-16 22:16:37 -0400

    Update gulp task for windows env. Update menu icon for CPT

--- austeve-services.php
+++ austeve-services.php
@@ -62,6 +62,7 @@ function austeve_create_services_post_type() {
 		'exclude_from_search' => false,
 		'publicly_queryable'  => true,
 		'capability_type'     => 'page',
+		'menu_icon'			  => 'dashicons-hammer'
 	);
 	
 	// Registering your Custom Post Type
